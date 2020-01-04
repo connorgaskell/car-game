7ad8faa66e678230226e35e27da599fcfb559026
connorgaskell <48499463+connorgaskell@users.noreply.github.com>
2020-01-04 13:59:58 +0000

    Implemented basic map generation and new models

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,12 +3,23 @@
 public class CameraControl : MonoBehaviour {
     public Transform target;
     
+    [Range(-10.0f, 10.0f)]
     public float speed = 1.0f;
+
+    [Range(-25.0f, 25.0f)]
+    public float xOffset = 0.0f;
+
+    [Range(-25.0f, 25.0f)]
+    public float zOffset = 0.0f;
+
+    [Range(0.0f, 25.0f)]
+    public float yPosition = 5.0f;
     
     void LateUpdate () {        
         Vector3 position = transform.position;
-        position.x = Mathf.Lerp(transform.position.x, target.position.x, speed * Time.deltaTime);
-        position.z = Mathf.Lerp(transform.position.z, target.position.z - 2.5f, speed * Time.deltaTime);
+        position.x = Mathf.Lerp(transform.position.x, target.position.x + xOffset, speed * Time.deltaTime);
+        position.y = yPosition;
+        position.z = Mathf.Lerp(transform.position.z, target.position.z - zOffset, speed * Time.deltaTime);
         
         transform.position = position;
         transform.LookAt(target);
--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -3,12 +3,19 @@
 public class GenerateMap : MonoBehaviour {
 
     public Transform groundPrefab;
+    public Transform player;
+
+    Transform lastInstantiated;
 
     void Start() {
-        
+        for (int i = 0; i < 25; i++) {
+            lastInstantiated = Instantiate(groundPrefab, new Vector3(0, 0, (i * (groundPrefab.localScale.y * 2))), groundPrefab.rotation);
+        }
     }
 
     void Update() {
-        
+        if(Vector3.Distance(player.position, lastInstantiated.position) < 1000) {
+            lastInstantiated = Instantiate(groundPrefab, new Vector3(0, 0, (lastInstantiated.position.z + (groundPrefab.localScale.y * 2))), groundPrefab.rotation);
+        }
     }
 }
